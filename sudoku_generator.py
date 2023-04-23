ec0105ec67bb9f178bb1554fc9045db88dfd43ec
SomaElefanti <elefantisoma@ufl.edu>
2023-04-22 22:45:04 -0400

    Merge remote-tracking branch 'origin/main'

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -173,6 +173,7 @@ class Cell:
         self.row = row
         self.col = col
         self.screen = screen
+        self.sketched_value = None
 
     def set_cell_value(self, value):
         self.value = value
@@ -180,8 +181,49 @@ class Cell:
     def set_sketched_value(self, value):
         self.sketched_value = value
 
-    
-    pass
+    def draw(self):
+        pass
+
+
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+
+    def draw(self):
+        pass
+
+    def select(self, row, col):
+        pass
+
+    def click(self, x, y):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self, value):
+        pass
+
+    def place_number(self, value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
 
 '''
 DO NOT CHANGE
